41b7aa521c2ea0c3ae5c37229001f9159ec0b9ca
Son <sitthavee.t@gmail.com>
2025-02-09 12:37:15 +0700

    add metadata website with opengraph image

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,7 +13,28 @@ const notoSans = Noto_Sans({
 
 export const metadata: Metadata = {
   title: "Bai Shi - Whiterock",
-  description: "Your personal investment agent",
+  description: "Your Personal Financial Advisory Agent. Helping you reach your investing goals through on-chain economy",
+  openGraph: {
+    title: "Bai Shi - Whiterock",
+    description: "Your Personal Financial Advisory Agent. Helping you reach your investing goals through on-chain economy",
+    siteName: 'Bai Shi - Whiterock',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Bai Shi - Whiterock',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Bai Shi - Whiterock",
+    description: "Your Personal Financial Advisory Agent. Helping you reach your investing goals through on-chain economy",
+    images: ['/opengraph-image.png'],
+  },
 };
 
 export default async function RootLayout({
